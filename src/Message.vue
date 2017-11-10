7e6d183992d5fd66ffd81217340f54bbf959cfde
arusu <arusu@sehcomp.ca>
2017-11-10 10:04:22 -0500

    Added a couple components

---
+++ src/Message.vue
@@ -0,0 +1,18 @@
+﻿<template>
+   <div>
+      <h1>This is a great Message!</h1>
+      <app-input></app-input>
+   </div>
+</template>
+
+
+<script>
+   import Input from './Input.vue'
+   
+   export default {
+      components: {
+         'app-input' : Input
+      }
+   }
+</script>
+
