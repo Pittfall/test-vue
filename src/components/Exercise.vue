3b17cccf10baed935d8a503add026d5647b7e923
arusu <arusu@sehcomp.ca>
2017-11-16 14:50:07 -0500

    Added color styles to page

--- src/components/Exercise.vue
+++ src/components/Exercise.vue
@@ -37,5 +37,53 @@ export default {
 </script>
 
 <style>
-
+.primary {
+ background-color:#2196F3 !important;
+ border-color:#2196F3 !important;
+}
+.primary--text {
+ color:#2196F3 !important;
+}
+.secondary {
+ background-color:#424242 !important;
+ border-color:#424242 !important;
+}
+.secondary--text {
+ color:#424242 !important;
+}
+.accent {
+ background-color:#82B1FF !important;
+ border-color:#82B1FF !important;
+}
+.accent--text {
+ color:#82B1FF !important;
+}
+.error {
+ background-color:#FF5252 !important;
+ border-color:#FF5252 !important;
+}
+.error--text {
+ color:#FF5252 !important;
+}
+.info {
+ background-color:#2196F3 !important;
+ border-color:#2196F3 !important;
+}
+.info--text {
+ color:#2196F3 !important;
+}
+.success {
+ background-color:#4CAF50 !important;
+ border-color:#4CAF50 !important;
+}
+.success--text {
+ color:#4CAF50 !important;
+}
+.warning {
+ background-color:#FFC107 !important;
+ border-color:#FFC107 !important;
+}
+.warning--text {
+ color:#FFC107 !important;
+}
 </style>
